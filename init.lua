b0d095c18e1685d5a4e256a70d6a06ae28dcfa32
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2022-12-24 21:52:04 +0100

    Delete some comments

--- init.lua
+++ init.lua
@@ -33,20 +33,16 @@ local config = {
     "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
     "    ██   ████   ████   ██ ██      ██",
   },
-
-  -- Default theme configuration
   default_theme = {
-    -- Modify the color palette for the default theme
     colors = {
       fg = "#abb2bf",
       bg = "#1e222a",
     },
-    highlights = function(hl) -- or a function that returns a new table of colors to set
+    highlights = function(hl)
       local C = require "default_theme.colors"
 
       hl.Normal = { fg = C.fg, bg = C.bg }
 
-      -- New approach instead of diagnostic_style
       hl.DiagnosticError.italic = true
       hl.DiagnosticHint.italic = true
       hl.DiagnosticInfo.italic = true
@@ -96,9 +92,7 @@ local config = {
       timeout_ms = 1000,
     },
     mappings = {
-      n = {
-        -- ["<leader>lf"] = false -- disable formatting keymap
-      },
+      n = {},
     },
     ["server-settings"] = {
       yamlls = {
