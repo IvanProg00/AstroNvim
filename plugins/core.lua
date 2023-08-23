4d6bb4edfc0c0fcd126e621bfd526f28e918fd9b
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-08-23 14:30:09 +0200

    Add gitsigns additional config

--- plugins/core.lua
+++ plugins/core.lua
@@ -12,6 +12,15 @@ return {
         [[  █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ ]],
         [[ ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  ]],
       }
+
+      return opts
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = function(_, opts)
+      opts.current_line_blame = true
+
       return opts
     end,
   },
